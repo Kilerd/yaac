0d1313a6f32a694e12cfbdaa4e4b12b269b3fc4f
Kilerd Chan <blove694@gmail.com>
2023-10-30 14:55:08 +0800

    feat: load from environment variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+use std::cmp::{max};
+use std::fmt::{Display, format, Formatter};
 use std::path::PathBuf;
 use std::str::FromStr;
+use itertools::{EitherOrBoth, Itertools, Position};
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
+use toml::{Table, Value};
 
 pub struct ConfigLoader {
     file_base_name: String,
@@ -17,13 +21,113 @@ impl ConfigLoader {
     }
 
     pub fn construct<'de, T: Deserialize<'de>>(self) -> Result<T, Box<dyn std::error::Error>> {
-        dbg!(&self.file_base_name);
-        let profile : Option<String>= None;
+        // load from file
+        let profile: Option<String> = None;
         let profile_string = profile.map(|it| format!("_{}", it)).unwrap_or("".to_string());
         let buf = PathBuf::from_str(&format!("{}{}.toml", &self.file_base_name, profile_string))?;
         let file_content = std::fs::read_to_string(buf)?;
-        let result:toml::Value = toml::from_str(&file_content)?;
+        let mut result: toml::Value = toml::from_str(&file_content)?;
+
+        // load from env
+        let prefix = format!("{}_", &self.environment_prefix);
+        let environments: Vec<toml::Value> = std::env::vars().into_iter().filter(|(key, value)| key.starts_with(&prefix))
+            .map(|(key, value)| (key.strip_prefix(&prefix).unwrap_or(&key).to_owned(), value))
+            .filter(|(key, value)| !key.is_empty())
+            .map(|(key, value)| build_toml_value(key.to_lowercase(), value))
+            .collect();
+
+        // merge all values
+        for environment_value in environments {
+            result = merge_two_value(result, environment_value, "$")?;
+        }
+
         Ok(result.try_into()?)
+    }
+}
+
+fn build_toml_value(key: String, value: String) -> toml::Value {
+    let split = key.split("_").into_iter().collect_vec();
+
+    let mut rev = split.into_iter().rev();
+    let first = rev.next().unwrap();
+    let value1 = Value::String(value.to_owned());
+    let mut accr = Table::new();
+    accr.insert(first.to_owned(), value1);
+    let accr = Value::Table(accr);
+    let ret = rev.fold(accr, |accr, text| {
+        let mut map = Table::new();
+        map.insert(text.to_owned(), accr);
+        Value::Table(map)
+    });
+    ret
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Error {
+    pub path: String,
+    pub existed_type: &'static str,
+    pub appended_type: &'static str,
+}
+impl Error {
+    pub fn new(path: String, existed_type: &'static str, appended_type: &'static str) -> Self {
+        Self {
+            path,
+            existed_type,
+            appended_type,
+        }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "merge fail, path={}, existed type={} appended type={}", self.path, self.existed_type, self.appended_type)
+    }
+}
+
+impl std::error::Error for Error {
+
+}
+
+
+fn merge_into_table_inner(
+    value: &mut Table,
+    other: Table,
+    path: &str,
+) -> Result<(), Error> {
+    for (name, inner) in other {
+        if let Some(existing) = value.remove(&name) {
+            let inner_path = format!("{path}.{name}");
+            value.insert(name, merge_two_value(existing, inner, &inner_path)?);
+        } else {
+            value.insert(name, inner);
+        }
+    }
+    Ok(())
+}
+fn merge_two_value(base: Value, append: Value, path: &str) -> Result<Value, Error> {
+    match(base, append) {
+        (Value::String(_), Value::String(inner)) => Ok(Value::String(inner)),
+        (Value::Integer(_), Value::Integer(inner)) => Ok(Value::Integer(inner)),
+        (Value::Float(_), Value::Float(inner)) => Ok(Value::Float(inner)),
+        (Value::Boolean(_), Value::Boolean(inner)) => Ok(Value::Boolean(inner)),
+        (Value::Datetime(_), Value::Datetime(inner)) => Ok(Value::Datetime(inner)),
+        (Value::Array(existing), Value::Array(inner)) => {
+            let mut ret = Vec::with_capacity(max(existing.len(), inner.len()));
+            for pair in existing.into_iter().enumerate().zip_longest(inner.into_iter().enumerate()) {
+               let element =  match pair {
+                    EitherOrBoth::Both(l, r) => merge_two_value(l.1, r.1, &format!("{}.[{}]", path, l.0))?,
+                    EitherOrBoth::Left(l) => l.1,
+                    EitherOrBoth::Right(r) => r.1,
+                };
+                ret.push(element);
+            }
+            Ok(Value::Array(ret))
+        }
+        (Value::Table(mut existing), Value::Table(inner)) => {
+            merge_into_table_inner(&mut existing, inner, path)?;
+            Ok(Value::Table(existing))
+        }
+        (v, o) => Err(Error::new(path.to_owned(), v.type_str(), o.type_str())),
     }
 }
 
@@ -40,7 +144,7 @@ mod tests {
     fn should_load_config_from_toml_file() {
         #[derive(Debug, Deserialize)]
         struct Config {
-            a: i32
+            a: i32,
         }
         let dir = tempdir().unwrap();
         let file_path = dir.path().join("application.toml");
@@ -55,8 +159,26 @@ mod tests {
     }
 
     #[test]
-    fn should_load_config_from_given_environment() {}
+    fn should_load_config_from_given_environment() {
+        #[derive(Debug, Deserialize)]
+        struct Config {
+            value: String,
+        }
+
+        temp_env::with_var("APP_VALUE", Some("value here"), || {
+            let dir = tempdir().unwrap();
+            let file_path = dir.path().join("application.toml");
+            let mut tmp_file = File::create(file_path).unwrap();
+            writeln!(tmp_file, "a = 123").unwrap();
+
+            let buf = dir.path().join("application").to_str().unwrap().to_string();
+            let config_loader = ConfigLoader::new(buf, "APP");
+            let config: Config = config_loader.construct().unwrap();
+
+            assert_eq!("value here", config.value);
+        });
+    }
 
     #[test]
-    fn should_load_config_from_hierachy_files() {}
+    fn should_load_config_from_hierarchy_files() {}
 }
